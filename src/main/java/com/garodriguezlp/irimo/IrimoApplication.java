a5874d8dc93daf9316dce1f395c4ba79fc937cf6
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2024-10-05 21:07:11 -0500

    Resolves #1: Initialize Spring Boot project structure

---
+++ src/main/java/com/garodriguezlp/irimo/IrimoApplication.java
@@ -0,0 +1,13 @@
+package com.garodriguezlp.irimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrimoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IrimoApplication.class, args);
+  }
+
+}
