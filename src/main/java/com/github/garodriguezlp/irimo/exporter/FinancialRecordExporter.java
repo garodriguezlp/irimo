dff0bca41d869480f1a1e6b4ca1f35de7c634fc3
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2025-02-22 01:10:58 -0500

    --wip-- [skip ci]

--- src/main/java/com/github/garodriguezlp/irimo/exporter/FinancialRecordExporter.java
+++ src/main/java/com/github/garodriguezlp/irimo/exporter/FinancialRecordExporter.java
@@ -4,8 +4,10 @@ import com.github.garodriguezlp.irimo.domain.FormattedFinancialRecord;
 import com.github.garodriguezlp.irimo.service.Identifiable;
 import java.util.List;
 
-// @todo: Future implementations of this interface will combine a writer (e.g., CSVWriter) with an OutputStream.
-//        Consider using Writer, OutputStream, or Appendable types, and take inspiration from Jackson to create a flexible and maintainable solution.
+// @todo: Future implementations of this interface should integrate a custom writer capable of
+//        writing a list of financial records (e.g., CSVWriter) with an appropriate output stream.
+//        Consider utilizing Writer, OutputStream, or Appendable types, and draw inspiration from
+//        libraries such as Jackson to create a flexible and maintainable solution.
 public interface FinancialRecordExporter extends Identifiable {
 
   void export(List<FormattedFinancialRecord> financialRecords);
